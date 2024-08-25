ad17c9ff854558f1acb26fba8508c101fd6ed884
ahh01 <145448378+ahh01@users.noreply.github.com>
2024-08-25 19:57:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//Kalla på Element eller HÄmta
+const addbtn = document.getElementById("addBtn")
+const inputList = document.getElementById("inputlist")
+const objectList = document.getElementById("objectList")
+
+// behöver lägga till händelse/ ge uppgifter
+// fastnat på tillvägagånsätt
+
+/* const addBtn("").addEventListner("") */
+/* const handler("addbtn") {
+} */
+
+addbtn.addEventListener("click", function(){
+const inputText = inputList.Value; 
+} )
+
+// ny elemnt för lista
+
+
